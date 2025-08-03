00d0b09209dc8c25a6797e2e953acea046b2d1f4
Leonardo Pinheiro Monteiro <leonardo.pinheiro.monteiro@gmail.com>
2025-08-03 14:23:32 -0300

    Ajustes nas configurações.

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import { app } from './app'
 
 app
   .listen({
-    port: env.PORT,
+    port: PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP Server running!!!')
